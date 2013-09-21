9d0a58058bef009473eac2d9f63761a6bc8bc568
Rachit Rawat <rachit.rc96@gmail.com>
2013-09-21 17:31:36 +0530

    Add more GPU scaling levels

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -121,6 +121,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
+			.gpu_freq = 400000000,
+			.bus_freq = 3,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 320000000,
 			.bus_freq = 2,
 			.io_fraction = 33,
@@ -131,12 +136,17 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 100,
 		},
 		{
+			.gpu_freq = 128000000,
+			.bus_freq = 1,
+			.io_fraction = 100,
+		},
+		{
 			.gpu_freq = 27000000,
 			.bus_freq = 0,
 		},
 	},
 	.init_level = 1,
-	.num_levels = 4,
+	.num_levels = 6,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
